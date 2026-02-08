50bf65f9624ea425c99d24b5cd2ed9e237d1e473
Kenza Piter <kenza.piter@gmail.com>
2026-02-08 14:49:11 +0100

    localhost removed from baseurl

--- src/app/HomeServer.tsx
+++ src/app/HomeServer.tsx
@@ -1,9 +1,9 @@
 import HomeClient from "./HomeClient";
 
 export default async function HomeServer() {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  // const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
-  const res = await fetch(`${baseUrl}/api/weddinginfos`, {
+  const res = await fetch(`/api/weddinginfos`, {
     cache: "no-store",
   });
   if (!res.ok) throw new Error("Failed to fetch wedding info");
